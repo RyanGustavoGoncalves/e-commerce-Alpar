edbd5ce45c37f2e7006b823eed261c7bf1773234
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2024-05-02 11:08:00 -0300

    Refactor AngularJS module and controller, update ng-app attribute, and add product submission functionality in home page

--- public/home/assets/scripts/main.js
+++ public/home/assets/scripts/main.js
@@ -1,5 +1,27 @@
 const app = angular.module('homeApp', []);
 
 app.controller('homeController', function ($scope, $http) {
-    console.log("homeController");
+    $scope.role = JSON.parse(localStorage.getItem('user')).role;
+    $scope.name = "";
+    $scope.description = "";
+    $scope.price = "";
+    $scope.imgUrl = "";
+    $scope.closed = false;
+
+    $scope.submitProduct = () => {
+        $http.post("http://localhost:3000/api/v1/product", {
+            Headers: {
+                Authorization: `Bearer ${JSON.parse(localStorage.getItem('user')).token}`
+            },
+            name: $scope.name,
+            description: $scope.description,
+            price: $scope.price,
+            imgUrl: $scope.imgUrl,
+            closed: $scope.closed,
+        }).then((response) => {
+            console.log(response);
+        }).catch((error) => {
+            console.log(error);
+        });
+    };
 });

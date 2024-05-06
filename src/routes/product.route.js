fad35d734f2ec58c54e95b8a694e5c1758650871
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2024-05-06 11:15:05 -0300

    refactor: Add adminMiddleware to product routes
    The code changes in `product.route.js` add the `adminMiddleware` to the `POST` and `DELETE` routes of the product API. This middleware ensures that only admin users can perform these operations, adding an extra layer of security to the application. This commit improves the authorization process for managing products in the virtual store.

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { ProductController } from "../controller/Product.controller.js";
+import { adminMiddleware } from "../infra/middleware/adminMiddleware.js";
 
 export const productRouter = Router();
 const productController = new ProductController();
 
-productRouter.post('/', productController.saveProduct);
+productRouter.post('/', adminMiddleware, productController.saveProduct);
 productRouter.get('/', productController.getProducts);
-productRouter.delete('/:id', productController.deleteProduct);
-productRouter.put('/:id', productController.updateProduct);
+productRouter.delete('/:id', adminMiddleware, productController.deleteProduct);
+productRouter.put('/:id', adminMiddleware, productController.updateProduct);

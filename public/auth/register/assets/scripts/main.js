22d4217cddd5ebe319f0ad2779ad56f09b12af9b
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2024-05-05 23:21:47 -0300

    feat: Add confirmation email and password fields to registration form
    The code changes in `main.js` add two new fields to the registration form: `confirmEmail` and `confirmPassword`. These fields allow users to confirm their email and password entries, ensuring accuracy and preventing errors during registration. If the confirmation values do not match the original values, an alert is displayed to notify the user. This commit enhances the registration process by improving data validation and user experience.

--- public/auth/register/assets/scripts/main.js
+++ public/auth/register/assets/scripts/main.js
@@ -4,15 +4,27 @@ app.controller('registerController', function ($scope, $http) {
     $scope.username = "";
     $scope.email = "";
     $scope.password = "";
-    
+    $scope.confirmEmail = "";
+    $scope.confirmPassword = "";
+
     $scope.submit = () => {
+        if ($scope.email !== $scope.confirmEmail) {
+            alert("Confirmation email does not match.");
+            return;
+        }
+
+        if ($scope.password !== $scope.confirmPassword) {
+            alert("Confirmation password does not match.");
+            return;
+        }
+
         console.log($scope.username, $scope.email, $scope.password);
         $http.post("http://localhost:3000/api/v1/register", {
             username: $scope.username,
             password: $scope.password,
             email: $scope.email,
         })
-        .then((res) => {
+            .then((res) => {
                 window.location.href = "/auth/login";
             })
             .catch((error) => {

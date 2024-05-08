59e0cf4b6acff492a192905dffeb2b842f7fef29
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2024-05-08 10:00:05 -0300

    refactor: Update user route to require admin privileges for getUser
    The code changes in `src/routes/user.route.js` modify the `/get` route to require admin privileges in addition to user authentication for accessing user information. This refactor enhances the security of the application by restricting access to user data to only authorized administrators.

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { authenticateUser } from "../infra/middleware/authMiddleware.js";
 import { UserController } from "../controller/user.controller.js";
+import { adminMiddleware } from "../infra/middleware/adminMiddleware.js";
 
 export const userRouter = Router();
 const userController = new UserController();
 
 userRouter.post('/register', userController.registerUser);
 userRouter.post('/login', userController.loginUser);
-userRouter.get('/get', authenticateUser, userController.getUser);
+userRouter.get('/get', authenticateUser, adminMiddleware, userController.getUser);
 userRouter.get('/get/byToken', authenticateUser, userController.getUserByToken);
 userRouter.delete('/delete/:id', authenticateUser, userController.deleteUser);
 userRouter.delete('/delete', authenticateUser, userController.deleteAllUsers);
